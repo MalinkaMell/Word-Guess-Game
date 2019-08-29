4df1cca9c0c5a82d3736419bd3482ba90dd7f324
Maimell <malinka77@gmail.com>
2019-08-28 20:56:41 -0700

    haven't change much here

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,114 @@
 
 //creating array of words(objects)
 let worldToGuess = [
-    { word_to_guess: "lasagna", imgURL: "lasagna.jpg", helpImg: "lasagna_sheets.jpg"},
-    { word_to_guess: "parmigiana", imgURL: "parmigiana.jpg", helpImg: "melanzana.jpg"},
-    { word_to_guess: "carbonara", imgURL: "carbonara.jpg", helpImg: "pancetta.jpg"}
+    {
+        word_to_guess: "lasagna",
+        imgURL: "lasagna.jpg",
+        helpImg: "lasagna_sheets.jpg",
+        description: "This dish is made of stacked layers of pasta alternated with sauces and ingredients such as meats, vegetables and cheese"
+    },
+    {
+        word_to_guess: "parmigiana",
+        imgURL: "parmigiana.jpg",
+        helpImg: "melanzana.jpg",
+        description: "This dish is made with a shallow or deep-fried sliced eggplant filling, layered with cheese and tomato sauce, then baked"
+    },
+    {
+        word_to_guess: "carbonara",
+        imgURL: "carbonara.jpg",
+        helpImg: "pancetta.jpg",
+        description: "Italian pasta dish from Rome made with egg, hard cheese, guanciale, and black pepper"
+    }
 ];
 
-//randomlly choosing the word to guess from array of objects
-let randomWord = Math.floor(Math.random() * worldToGuess.length);
-
-//saving word to guess in a new variable
-let displayeddWord = worldToGuess[randomWord].word_to_guess;
-
-//creating an array of keys pressed by user and storing it in a new array 
+let questionIndex = 0;
+let displayeddWord = worldToGuess[questionIndex].word_to_guess;
 let userGuessedLetters = [];
 let hiddenWordMatch = [];
+let countClicks = 0;
+let wins = 0;
+
 for (let j = 0; j < displayeddWord.length; j++) {
     hiddenWordMatch.push("_");
 }
 
 function playMusic() {
- 
     let audio = new Audio("./assets/music/tarantella.mp3");
     audio.play();
-  }
+}
 
-function wordComplete() {
-    for (let i = 0; i < hiddenWordMatch.length; i++) {
-        if (hiddenWordMatch.indexOf("_") === -1) {
-            console.log("the word is complete!");
-            playMusic();
-            document.getElementById("image").src = "./assets/images/" + worldToGuess[randomWord].imgURL;
-        }
+function endGame() {
+    if (hiddenWordMatch.indexOf("_") === -1) {
+        console.log("the word is complete!");
+        console.log("before " + questionIndex);
+        playMusic();
+        hiddenWordMatch = [];
+        userGuessedLetters = [];
+        document.getElementById("image").src = "./assets/images/" + worldToGuess[questionIndex].imgURL;
+        wins++;
+        stopCount = countClicks;
+        countClicks = 10;
+        document.getElementById("guesses_div").style.display = "none";
+        document.getElementById("guesses_used_div").style.display = "block";
+        document.getElementById("description").style.display = "none";
+        document.getElementById("description_next").style.display = "block";
+        document.getElementById("guesses_used").innerHTML = stopCount;
+        document.getElementById("description_next").innerHTML = "Delizioso!!!<br><small>Press any key to restart</small>";
+        questionIndex += 1;
+        console.log("after " + questionIndex);
+        startGame();
+    }
+
+}
+
+let verifyLetter = function (x, s) {
+    let letterIsIn = x.indexOf(s); //controlling if user already pressed that key, adding a new key to array if it hasn't been pressed yet
+
+    if (letterIsIn === -1) {
+        x.push(s);
+        console.log(s);
+        countClicks++;
+    }
+    else {
+        console.log("not adding!");
     }
 }
 
-//counting clicks
+function startGame() {
+    document.onkeyup = function (event) {
 
-    let countClicks = 0;
+        if (countClicks < 10) {
+        
+        let userGuess = event.key.toLowerCase(); // Determines which key was pressed.
 
-
-
-document.onkeyup = function (event) {
-    if (countClicks < 15) {
-        // Determines which key was pressed.
-        var userGuess = event.key.toLowerCase();
-
-        //controlling if user already pressed that key, adding a new key to array if it hasn't been pressed yet
-        var letterIsIn = userGuessedLetters.indexOf(userGuess);
-        //console.log(letterIsIn);
-        if (letterIsIn === -1) {
-
-            userGuessedLetters.push(userGuess);
-            console.log(userGuess);
-            countClicks++
-        }
-        else {
-            console.log("not adding!");
-        }
+        verifyLetter(userGuessedLetters, userGuess);
 
         document.getElementById("letters").innerHTML = userGuessedLetters + ", ";
 
         //checking if the letters guessed by user is present in displayed word
 
         for (let i = 0; i < displayeddWord.length; i++) {
-
-
             if (displayeddWord.charAt(i) === userGuess) {
-                //let tdt = displayeddWord.indexOf(userGuess);
-                //console.log("found the letter at position " + tdt);
                 hiddenWordMatch[i] = userGuess;
-
             }
-
 
         }
 
+
     }
     else {
-        return;
+        document.getElementById("description").innerHTML = "Try again!";
+            return;
     }
 
-
-
-    document.getElementById("guesses").innerHTML = countClicks;
-    document.getElementById("test").innerHTML = hiddenWordMatch.join(" ");
-  //  document.getElementById("word").innerHTML = displayeddWord;
-document.getElementById("image").src = "./assets/images/" + worldToGuess[randomWord].helpImg;
-wordComplete();
+        document.getElementById("test").innerHTML = hiddenWordMatch.join(" ");
+        document.getElementById("image").src = "./assets/images/" + worldToGuess[questionIndex].helpImg;
+        document.getElementById("description").innerHTML = worldToGuess[questionIndex].description;
+        document.getElementById("guesses").innerHTML = countClicks;
+        document.getElementById("wins").innerHTML = wins;
+        endGame();
+    }
 
 }
-
-//document.getElementById("test").innerHTML = hiddenWordMatch.join(" ");
-
-//function to choose random word from the array
-
-
-
+startGame();
+document.getElementById("test").innerHTML = hiddenWordMatch.join(" ");
